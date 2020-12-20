824a9975443718c0a219f4322fb4c4f0be3d99f9
charliemikels <charliemikels@gmail.com>
2020-12-20 12:13:58 -0500

    Ran v fmt on everything.

--- window.v
+++ window.v
@@ -1,13 +1,13 @@
 module vtui
 
 import term
-// import time
 
+// import time
 // Window is in charge of drawing the UI (and managing the layout).
 struct Window {
-	box_characters 	map[string]string
+	box_characters map[string]string
 mut:
-	child		 				[]&Widget	// An array due to a bug. When interfaces are smarter, just use &Widget
+	child          []&Widget // An array due to a bug. When interfaces are smarter, just use &Widget
 }
 
 pub struct WindowConfig {
@@ -16,11 +16,10 @@ pub struct WindowConfig {
 pub fn new_window(w WindowConfig, child &Widget) &Window {
 	mut widget_list := []&Widget{}
 	widget_list << child
-
 	mut box_style := map[string]string{}
 	// '─' is 'ew' bc the ends of the line connect to the east and west sides
 	// The naming order is always n, s, e, then w
-	if true {	// TODO: ask if terminal can handle these characters (or launch param)
+	if true { // TODO: ask if terminal can handle these characters (or launch param)
 		box_style['ns'] = '│'
 		box_style['ew'] = '─'
 		box_style['ne'] = '└'
@@ -29,8 +28,7 @@ pub fn new_window(w WindowConfig, child &Widget) &Window {
 		box_style['sw'] = '┐'
 		box_style['nse'] = '├'
 		box_style['nsw'] = '┤'
-	}
-	else {
+	} else {
 		box_style['ns'] = '|'
 		box_style['ew'] = '-'
 		box_style['ne'] = '+'
@@ -40,10 +38,9 @@ pub fn new_window(w WindowConfig, child &Widget) &Window {
 		box_style['nse'] = '|'
 		box_style['nsw'] = '|'
 	}
-
 	return &Window{
 		// title: w.title
-		child: 					widget_list
+		child: widget_list
 		box_characters: box_style
 	}
 }
@@ -51,9 +48,9 @@ pub fn new_window(w WindowConfig, child &Widget) &Window {
 pub fn (win Window) draw(width int, height int, x_off int, y_off int) {
 	rendered_child := win.child[0].render(width, height)
 	term.clear()
-	for w in 0..width {
-		for h in 0..height {
-			term.set_cursor_position({x: w + 1 + x_off, y: h + 1 + y_off})
+	for w in 0 .. width {
+		for h in 0 .. height {
+			term.set_cursor_position(x: w + 1 + x_off, y: h + 1 + y_off)
 			print(rendered_child[h][w])
 		}
 	}
