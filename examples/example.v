ce8c9568e22010effd4e606fe36b6cad72202e36
charliemikels <charliemikels@gmail.com>
2021-01-02 11:48:50 -0500

    more tests...

--- examples/example.v
+++ examples/example.v
@@ -8,45 +8,43 @@ import charliemikels.vtui as ui
 fn main() {
 
 	// window := ui.new_window({},
-		// ui.new_box({title: 'Outer Row Test'},		// Removing this gets a C error if new row starts with anything other than a box
-			// ui.new_row({}, [
-				// ui.new_empty_widget(),
-				// ui.new_box({title: 'Row Test'},
-				// 	ui.new_empty_widget()
-				// // 	// ui.new_table({
-				// // 	// 	data: [
-				// // 	// 		['Fruit', 'Goodness'],
-				// // 	// 		['Apples', 'Very good'],
-				// // 	// 		['Banana', 'Less good'],
-				// // 	// 		['Sandwich', 'Not a fruit but still good'],
-				// // 	// 	]
-				// // 	// })
-				// )
-				//
-				// ui.new_empty_widget(),
-				// // ui.new_table({
-				// // 	data: [
-				// // 		['Name', 'Friends'],
-				// // 		['Kevin', 'Derek'],
-				// // 		['Derek', 'Kevin, Daniel'],
-				// // 		['Daniel', 'Derek, Kevin\'s Brother'],
-				// // 		['Kevin\'s brother', 'Yes']
-				// // 	]
-				// })
-				// ])
-				// )		// outer box ends here.
-				// )
-	// app := ui.new_app({}, window)
-	// app := ui.new_app({},
+	// 	ui.new_box({title: 'Outer Row Test'},
+	// 		ui.new_table({
+	// 			data: [
+	// 				['Name', 'Friends'],
+	// 				['Kevin', 'Derek'],
+	// 				['Derek', 'Kevin, Daniel'],
+	// 				['Daniel', 'Derek, Kevin\'s Brother'],
+	// 				['Kevin\'s brother', 'Yes']
+	// 			]
+	// 		})
+	// 	)		// outer box ends here.
 	// )
+	// app := ui.new_app({}, window)
 	// app.run()
+
+	println('Compiles')
 
 	// DEBUG vvvv
 
 	// only_ew						:= ui.new_row({}, [ui.new_empty_widget()]) // works fine
-	box_after_ew			:= ui.new_row({}, [ui.new_empty_widget(), ui.new_box({}, ui.new_empty_widget() )])		// Only works if box_before_ew is uncommented.
-	box_before_ew			:= ui.new_row({}, [ui.new_box({}, ui.new_empty_widget() ), ui.new_empty_widget()])		// works fine
+	// box_after_ew			:= ui.new_row({}, [ui.new_empty_widget(), ui.new_box({}, ui.new_empty_widget() )])		// Only works if box_before_ew is uncommented.
+	// box_before_ew			:= ui.new_row({}, [ui.new_box({}, ui.new_empty_widget() ), ui.new_empty_widget()])		// works fine
 	// bow_wrapping_row	:= ui.new_box({}, ui.new_row({}, [ui.new_empty_widget()]) )														// works fine
+
+
+	// Set 2
+	mut box_after_ew_array := []ui.Widget{}
+	box_after_ew_array = [ui.new_empty_widget(), ui.new_box({}, ui.new_empty_widget() )]
+	// box_after_ew_array << ui.new_empty_widget()
+	// box_after_ew_array << ui.new_box({}, ui.new_empty_widget() )
+
+	// mut box_after_ew_array_2 := []ui.Widget{}
+	// box_after_ew_array_2 = [ui.Box{}, ui.EmptyWidget{}]
+
+	// box_after_ew_array_3 := [ui.Box{}, ui.EmptyWidget{}]
+
+	// box_before_ew_array := [ui.new_box({}, ui.new_empty_widget() ), ui.new_empty_widget()]
 
 	// DEBUG ^^^^
 
---
+++ examples/quicktests.v
@@ -0,0 +1,38 @@
+interface Pet {
+	speak() string
+}
+
+struct Dog {}
+fn (d Dog) speak() string { return 'woof' }
+fn (d Dog) trick() string { return '*rolls over*' }
+fn new_dog() Dog { return Dog{} }
+fn new_dog_pet() Pet { return Dog{} }
+
+struct Cat {}
+fn (c Cat) speak() string {	return 'meow' }
+fn new_cat() Cat { return Cat{} }
+fn new_cat_pet() Pet { return Cat{} }
+
+fn main() {
+	// test_1 Throws a reasonable V error (the array trys to be []Dog (First element) insted of []Pet )
+	// if new_dog/new_cat returns a Pet, then it throws a different error
+	// mut test_1 := [new_dog(), new_cat()]
+	// mut alt_test_1 := [Dog{}, Cat{}]	// assums Dog and Cat are public
+
+	// test_2 throws a C error (What I was running into)
+	mut test_2 := []Pet{}
+	test_2 = [new_dog(), new_cat()]
+	// mut alt_test_2 := []Pet{}
+	// alt_test_2 = [Dog{}, Cat{}]
+
+	// test_3 compiles fine, and causes test_2 to compile
+	// mut test_3 := []Pet{}
+	// test_3 << new_dog()
+	// test_3 << new_cat()
+
+	// compiles fine, and after running once, test 2 always compiled afterwords???
+	// // mut test_4 := [new_dog_pet(), new_cat_pet()]
+
+	println('Compiles')
+}
+
