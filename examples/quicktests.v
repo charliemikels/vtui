327224529737120345cdaef45d42a4ea241e7a16
charliemikels <charliemikels@gmail.com>
2021-01-02 14:40:27 -0500

    back to quicktest Dog with a friend (given in new_dog) compiles in test_3

--- examples/quicktests.v
+++ examples/quicktests.v
@@ -2,16 +2,23 @@ interface Pet {
 	speak() string
 }
 
-struct Dog {}
+struct Dog {
+	name string = 'no_name'
+	friend Pet
+}
 fn (d Dog) speak() string { return 'woof' }
 fn (d Dog) trick() string { return '*rolls over*' }
-fn new_dog() Dog { return Dog{} }
-fn new_dog_pet() Pet { return Dog{} }
+fn new_dog() Dog {
+	return Dog{
+		friend: new_cat()
+	}
+}
+// fn new_dog_pet() Pet { return Dog{} }
 
 struct Cat {}
 fn (c Cat) speak() string {	return 'meow' }
 fn new_cat() Cat { return Cat{} }
-fn new_cat_pet() Pet { return Cat{} }
+// fn new_cat_pet() Pet { return Cat{} }
 
 fn main() {
 	// test_1 Throws a reasonable V error (the array trys to be []Dog (First element) insted of []Pet )
@@ -20,19 +27,18 @@ fn main() {
 	// mut alt_test_1 := [Dog{}, Cat{}]	// assums Dog and Cat are public
 
 	// test_2 throws a C error (What I was running into)
-	mut test_2 := []Pet{}
-	test_2 = [new_dog(), new_cat()]
+	// mut test_2 := []Pet{}
+	// test_2 = [new_dog(), new_cat()]
 	// mut alt_test_2 := []Pet{}
 	// alt_test_2 = [Dog{}, Cat{}]
 
 	// test_3 compiles fine, and causes test_2 to compile
-	// mut test_3 := []Pet{}
-	// test_3 << new_dog()
-	// test_3 << new_cat()
+	mut test_3 := []Pet{}
+	test_3 << new_dog()
+	test_3 << new_cat()
 
 	// compiles fine, and after running once, test 2 always compiled afterwords???
 	// // mut test_4 := [new_dog_pet(), new_cat_pet()]
 
 	println('Compiles')
 }
-
